1faee9057f439fb9443ff4e8689f517bdda6d091
kyforprogram <kyforprogram@gmail.com>
2021-09-09 01:15:49 +0000

    deviseの修正と新規登録に名前を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #ログイン後の遷移先
+  # def after_sigin_in_path_for(resource)
+  #   user_path(current_user)
+  # end
+
+  protected
+  # 名前許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
