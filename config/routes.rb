8e89c986a2137804c0470532fea21837489d4f95
kyforprogram <kyforprogram@gmail.com>
2021-09-09 02:07:51 +0000

    ルーティング設定を編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-root "homes#top"
+root 'homes#top'
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#adminはサインインだけ
 devise_for :admins, controllers: {
   sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
 }
-devise_for :customers, controllers: {
-  sessions:      'customers/sessions',
-  passwords:     'customers/passwords',
-  registrations: 'customers/registrations'
+devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  registrations: 'users/registrations'
 }
 end
