aa7b572f497a612292d55d1f92ef32d062814dc9
kyforprogram <kyforprogram@gmail.com>
2021-09-14 11:01:34 +0000

    マイグレーションファイルを修正のテスト

--- db/migrate/20210913092718_create_direct_messages.rb
+++ db/migrate/20210913092718_create_direct_messages.rb
@@ -1,8 +1,8 @@
 class CreateDirectMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :direct_messages do |t|
-      t.integer :user_id
-      t.integer :room_id
+      t.references :user, foreign_key: true
+      t.references :room, foreign_key: true
       t.text :message
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_092840) do
+ActiveRecord::Schema.define(version: 2021_09_13_092817) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_09_13_092840) do
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_direct_messages_on_room_id"
+    t.index ["user_id"], name: "index_direct_messages_on_user_id"
   end
 
   create_table "entries", force: :cascade do |t|
@@ -37,6 +39,8 @@ ActiveRecord::Schema.define(version: 2021_09_13_092840) do
     t.integer "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_entries_on_room_id"
+    t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
