63080ca0da82747c1ce9a4d98d1da4e7919f03c6
kyforprogram <kyforprogram@gmail.com>
2021-09-26 07:27:43 +0000

    修正【homes controller】

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
 
   def top
-    @posts = Post.includes(:category, :user).order(created_at: :desc).limit(4)
+    @posts = Post.includes(:category, :user).order(created_at: :desc, post_id: :desc).limit(4)
     @categories = Category.where(ancestry: nil)
   end
 end
