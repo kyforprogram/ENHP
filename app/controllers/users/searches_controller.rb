cdb0be62b13a24707e75924251315e197c322594
kyforprogram <kyforprogram@gmail.com>
2021-09-26 14:44:06 +0000

    V top CSS app C search(n+1) add mp4

--- app/controllers/users/searches_controller.rb
+++ app/controllers/users/searches_controller.rb
@@ -4,13 +4,10 @@ class Users::SearchesController < ApplicationController
     @category = params[:category]
     search = params[:search]
     word = params[:word]
-
     if @category == "2"
       @posts = Post.includes(:category).search(search, word).page(params[:page]).per(6)
     else
       @user = User.search(search, word).page(params[:page]).per(6)
     end
   end
-
-
 end
