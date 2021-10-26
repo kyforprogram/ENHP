d5e71e017df2be82d351b35d1dd07f0fdde95f44
kyforprogram <kyforprogram@gmail.com>
2021-10-26 10:08:09 +0000

    トップページ変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,14 +3,15 @@ class HomesController < ApplicationController
   def top
     @posts = Post.includes(:user).first(4)#recentはpost.rbの１８行目
     # @posts = Post.all.recent.limit(4)
-    @categories = Category.where(ancestry: nil)
+    @root_categories = Category.where(ancestry: nil)
+    @categories = Category.all
   end
 
   def guest_sign_in
     user = User.find_or_create_by!(name: 'ゲストユーザー', email: 'guest@example.com') do |user|
       user.password = '123456'
-      user.company = "MMD WEBCAMP"
-      user.introduction = "MMDの卒業生です。SEを目指して日々修行してます"
+      user.company = "DMM WEBCAMP"
+      user.introduction = "DMMの卒業生です。SEを目指して日々修行してます"
     end
     sign_in user
     redirect_to user_path(current_user), notice: "ゲストユーザーとしてログイン"
