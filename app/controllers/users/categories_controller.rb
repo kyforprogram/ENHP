0110aa01efef88590e7edb407a35e074b584b8f6
kyforprogram <kyforprogram@gmail.com>
2021-10-31 09:37:49 +0000

    top page編集

--- app/controllers/users/categories_controller.rb
+++ app/controllers/users/categories_controller.rb
@@ -2,9 +2,9 @@ class Users::CategoriesController < ApplicationController
 before_action :authenticate_user!
 
   def show
-    @parents = Category.where(ancestry: nil)
     @category = Category.find(params[:id])
+    @parents = Category.where(ancestry: nil)
+    @posts = @category.set_posts.in
   end
-
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,26 @@
 class Category < ApplicationRecord
   has_ancestry
   has_many :posts, dependent: :destroy
+  
+  def set_posts
+    # 親カテゴリーの場合
+    if self.root?
+      start_id = self.indirects.first.id
+      end_id = self.indirects.last.id
+      posts = Post.where(category_id: start_id..end_id)
+      return posts
+  
+      # 子カテゴリーの場合
+    elsif self.has_children?
+      start_id = self.children.first.id
+      end_id = self.children.last.id
+      posts = Post.where(category_id: start_id..end_id)
+      return posts
+  
+      # 孫カテゴリーの場合
+    else
+      return self.posts
+    end
+  end
+  
 end
