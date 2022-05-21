024bd60bfd1e25a8f8cb2e645244313af1fb77e6
kyforprogram <kyforprogram@gmail.com>
2022-05-21 01:25:31 +0000

    guest useをランダムにしてみた

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -5,4 +5,6 @@ $(function() {
     $(this).next().slideToggle();
     $('#accordion p').not($(this)).next().slideUp();
   });
+  var doc0= document.getElementById("hello_world");  
+  doc0.innerHTML= "p Hello world テキストを表示します。"; 
 });
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -8,8 +8,16 @@ class HomesController < ApplicationController
   end
 
   def guest_sign_in
-    user = User.find_or_create_by!(name: 'ゲストユーザー', email: 'guest@example.com') do |user|
-      user.password = '123456'
+    random_guestname = SecureRandom.alphanumeric(10)
+    # @random_guestemail = SecureRandom.alphanumeric(10) + [*'a'..'z'].sample(1).join + [*'0'..'9'].sample(1).join
+    random_guestemail = SecureRandom.alphanumeric(10) + [*'a'..'z'].sample(1).join + [*'@'].sample(1).join + [*'0'..'9'].sample(1).join
+    puts random_guestemail
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    # user = User.find_or_create_by!(name: 'ゲストユーザー', email: 'guest@example.com') do |user|
+    # user = User.find_or_create_by!(name: @random_guestname, email: 'guest@example.com') do |user|
+    user = User.find_or_create_by!(name: random_guestname, email: random_guestemail) do |user|
+      user.password = SecureRandom.alphanumeric(10) + [*'a'..'z'].sample(1).join + [*'0'..'9'].sample(1).join
+      # user.password = '123456'
       user.company = "DMM WEBCAMP"
       user.introduction = "DMMの卒業生です。SEを目指して日々修行してます"
     end
